3004a893b2e959846a5c0820488f5577060e29f5
Wave2112 <wavemail2112@gmail.com>
2016-04-14 21:53:06 +0300

    Удалил лишнее

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,8 +6,6 @@ package ru.stqa.pft.sandbox;
 public class Point {
     public double x1;
     public double y1;
-    public double x2;
-    public double y2;
 
     public Point(double x1, double y1) {
         this.x1 = x1;
