1d48ff47331ebbaa478cf283aeefa42d2e8d01c6
Wave2112 <wavemail2112@gmail.com>
2016-04-14 22:06:16 +0300

    Задание №3: Подключить тестовый фреймворк и сделать первые тесты

--- sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistance.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.Point.*;
+
 /**
  * Created by Sergei on 10.04.2016.
  */
@@ -8,7 +10,7 @@ public class CalculateDistance {
     public static void main(String[] args) {
         Point p1 = new Point(5, 25);
         Point p2 = new Point(10, 15);
-        System.out.println("Расстояние между двумя точками равно " + Point.distance(p1, p2));
+        System.out.println("Расстояние между двумя точками равно " + distance(p1, p2));
     }
 }
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -1,10 +1,29 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 /**
  * Created by Sergei on 14.04.2016.
  */
 public class DistanceTests {
-    public void testDistance(){
-
+    /**
+     * Тесты метода вычисления расстояния между двумя точками
+     */
+    @Test
+    public void testDistance1() {
+        Point p1 = new Point(22, 33);
+        Point p2 = new Point(10, 17);
+        assertEquals(Point.distance(p1, p2), 20.0, "Ошибка вычисления расстояния между точками");
     }
+
+    @Test
+    public void testDistance2() {
+        Point p1 = new Point(10, 7);
+        Point p2 = new Point(1, 50);
+        assertEquals(Point.distance(p1, p2), 43.93176527297759, "Ошибка вычисления расстояния между точками");
+    }
+
 }
