507c82f9ed28ff806e9915f5799a1cf1978330a6
Wave2112 <wavemail2112@gmail.com>
2016-04-15 23:56:04 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестового набора

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ClientHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ClientHelper.java
@@ -7,45 +7,47 @@ import ru.stqa.pft.addressbook.model.ClientData;
 /**
  * Created by Sergei on 15.04.2016.
  */
-public class ClientHelper {
-    private FirefoxDriver wd;
+public class ClientHelper extends HelperBase {
+
     public ClientHelper(FirefoxDriver wd){
-        this.wd = wd;
+        super(wd);
     }
 
     public void fillClientForm(ClientData clientData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(clientData.getFirstName());
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(clientData.getMiddleName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(clientData.getLastName());
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(clientData.getNickName());
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(clientData.getCompany());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(clientData.getAddress());
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(clientData.getHome());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(clientData.getMobile());
-        wd.findElement(By.name("work")).click();
-        wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys(clientData.getWork());
-        wd.findElement(By.name("fax")).click();
-        wd.findElement(By.name("fax")).clear();
-        wd.findElement(By.name("fax")).sendKeys(clientData.getFax());
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.name("firstname"));
+        clear(By.name("firstname"));
+        type(By.name("firstname"),clientData.getFirstName());
+        click(By.name("middlename"));
+        clear(By.name("middlename"));
+        type(By.name("middlename"),clientData.getMiddleName());
+        click(By.name("lastname"));
+        clear(By.name("lastname"));
+        type(By.name("lastname"), clientData.getLastName());
+        click(By.name("nickname"));
+        clear(By.name("nickname"));
+        type(By.name("nickname"),clientData.getNickName());
+        click(By.name("company"));
+        clear(By.name("company"));
+        type(By.name("company"), clientData.getCompany());
+        click(By.name("address"));
+        clear(By.name("address"));
+        type(By.name("address"), clientData.getAddress());
+        click(By.name("home"));
+        clear(By.name("home"));
+        type(By.name("home"), clientData.getHome());
+        click(By.name("mobile"));
+        clear(By.name("mobile"));
+        type(By.name("mobile"), clientData.getMobile());
+        click(By.name("work"));
+        clear(By.name("work"));
+        type(By.name("work"), clientData.getWork());
+        click(By.name("fax"));
+        clear(By.name("fax"));
+        type(By.name("fax"), clientData.getFax());
+        click (By.xpath("//div[@id='content']/form/input[21]"));
     }
+
+
 
     public void initClientGeneration() {
         wd.findElement(By.linkText("add new")).click();
