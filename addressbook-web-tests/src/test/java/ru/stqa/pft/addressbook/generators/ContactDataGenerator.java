bc39b72083520b52bbe0b445dd628748858981d9
Wave2112 <wavemail2112@gmail.com>
2016-05-16 20:16:22 +0300

    try

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -16,65 +16,66 @@ import java.util.List;
 /**
  * Created by Sergei on 13.05.2016.
  */
-    public class ContactDataGenerator {
+public class ContactDataGenerator {
 
-        @Parameter(names = "-c", description = "Contact count")
-        public int count;
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
 
-        @Parameter(names = "-f", description = "Target file")
-        public String file;
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
 
-        @Parameter(names = "-d", description = "Data format")
-        public String format;
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
 
-        public static void main(String[] args) throws IOException {
-            ContactDataGenerator generator = new ContactDataGenerator();
-            JCommander jCommander = new JCommander(generator);
-            try {
-                jCommander.parse(args);
-            } catch (ParameterException ex) {
-                jCommander.usage();
-                return;
-            }
-            generator.run();
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
         }
+        generator.run();
+    }
 
-        private void run() throws IOException {
-            List<ContactData> contacts = generateContacts(count);
-            if (format.equals("csv")) {
-                saveAsCsv(contacts, new File(file));
-            } else if (format.equals("xml")){
-                saveAsXml(contacts, new File(file));
-            } else {
-                System.out.println("Unrecognized format " + format);
-            }
-        }
-
-        private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
-            XStream xstream = new XStream();
-            xstream.processAnnotations(ContactData.class);
-            String xml = xstream.toXML(contacts);
-            Writer writer = new FileWriter(file);
-            writer.write(xml);
-            writer.close();
-        }
-
-        private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-            System.out.println(new File(".").getAbsoluteFile());
-            Writer writer = new FileWriter(file);
-            for (ContactData contact : contacts) {
-                writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getAddress(), contact.getMobilePhone()));
-            }
-            writer.close();
-        }
-
-        private List<ContactData> generateContacts(int count) {
-            List<ContactData> contacts = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                contacts.add(new ContactData().withFirstName(String.format("firstName %s", i))
-                        .withAddress(String.format("address %s", i))
-                        .withMobilePhone(String.format("mobilePhone %s", i)));
-            }
-            return contacts;
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        if (format.equals("csv")) {
+            saveAsCsv(contacts, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXml(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognized format " + format);
         }
     }
+
+    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
+    }
+
+    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsoluteFile());
+        try (Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s\n",
+                        contact.getFirstName(), contact.getAddress(), contact.getMobilePhone()));
+            }
+        }
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstName(String.format("firstName %s", i))
+                    .withAddress(String.format("address %s", i))
+                    .withMobilePhone(String.format("mobilePhone %s", i)));
+        }
+        return contacts;
+    }
+}
