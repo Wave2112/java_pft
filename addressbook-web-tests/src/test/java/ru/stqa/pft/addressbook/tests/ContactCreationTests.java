79f4da0e3e147b71626215fe2d2e76eca99099a5
Wave2112 <wavemail2112@gmail.com>
2016-05-20 00:09:49 +0300

    Создание контакта из конфига

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,12 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
+import static java.lang.System.getProperty;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -13,19 +18,36 @@ import static org.testng.Assert.assertEquals;
 public class ContactCreationTests extends TestBase {
 
 
+    private final Properties properties;
+
+    public ContactCreationTests() {
+        properties = new Properties();    }
+
     @Test
-    public void testContactCreation() {
+    public void testContactCreation() throws IOException {
+        String target = getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         app.goTo().homePage();
         Contacts before = app.contact().all();
         app.contact().initClientGeneration();
         File photo = new File("src/test/resources/stru.jpg");
         ContactData client = new ContactData()
-                .withFirstName("tesname").withMiddleName("213").withLastName("teeest").withNickName("test")
-                .withCompany("test").withAddress("sssss").withHomePhone("123").withMobilePhone("213131").withWorkPhone("4421")
-                .withFax("555").withDate("1111").withDate("1991").withAnnyversary("2313").withGroup("Test1")
-                .withEmail("Test1@test.ru")
-                .withEmail2("Test2@mail.ru")
-                .withEmail3("Test3@mail.ru")
+                .withFirstName(properties.getProperty("web.FirstName"))
+                .withMiddleName(properties.getProperty("web.MiddleName"))
+                .withLastName(properties.getProperty("web.LastName"))
+                .withNickName(properties.getProperty("web.NickName"))
+                .withCompany(properties.getProperty("web.Company"))
+                .withAddress(properties.getProperty("web.Address"))
+                .withHomePhone(properties.getProperty("web.HomePhone"))
+                .withMobilePhone(properties.getProperty("web.MobilePhone"))
+                .withWorkPhone(properties.getProperty("web.WorkPhone"))
+                .withFax(properties.getProperty("web.Fax"))
+                .withDate(properties.getProperty("web.Date"))
+                .withAnnyversary(properties.getProperty("web.Annyversary"))
+                .withGroup(properties.getProperty("web.Group"))
+                .withEmail(properties.getProperty("web.Email"))
+                .withEmail2(properties.getProperty("web.Email2"))
+                .withEmail3(properties.getProperty("web.Email3"))
                 .withPhoto(photo);
         app.contact().fillClientForm(client, true);
         app.contact().submitClientCreation();
