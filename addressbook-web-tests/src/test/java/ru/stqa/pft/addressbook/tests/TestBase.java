e1f59d3f93dc1b4bb08e959cb2f5e9837893df93
Wave2112 <wavemail2112@gmail.com>
2016-06-09 22:10:20 +0300

    verifyGroupUI

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -8,11 +8,18 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static java.lang.System.getProperty;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Sergei on 15.04.2016.
@@ -40,5 +47,28 @@ public class TestBase {
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m){
         logger.info("Stop test " + m.getName());
+    }
+
+    public void verifyGroupListUI(){
+        if (Boolean.getBoolean("verifyUI")) {
+        Groups dbGroups = app.db().groups();
+        Groups uiGroups = app.group().all();
+        assertThat(uiGroups, equalTo(dbGroups.stream()
+                .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                .collect(Collectors.toSet())));
+        }
+    }
+    public void verifyContactListInUI() {
+        if (Boolean.getBoolean ("verifyUI")) {
+            Contacts dbContacts = app.db().contacts();
+            Contacts uiContacts = app.contact().all();
+
+            assertThat(uiContacts, equalTo(dbContacts.stream()
+                    .map((g) -> new ContactData().withId(g.getId()).withFirstName(g.getFirstName()).withLastName(g.getLastName())
+                            .withWorkPhone(g.getWorkPhone().replaceAll("\\s", "").replaceAll("[-()]",""))
+                            .withHomePhone(g.getHomePhone().replaceAll("\\s", "").replaceAll("[-()]",""))
+                            .withMobilePhone(g.getMobilePhone().replaceAll("\\s", "").replaceAll("[-()]","")))
+                    .collect(Collectors.toSet())));
+        }
     }
 }
